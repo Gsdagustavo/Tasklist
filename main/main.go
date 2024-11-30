c92c7111ddea3379b3d435a334d32a70af458935
Gustavo Daniel <gugadanielalvez@gmail.com>
2024-11-29 22:14:35 -0300

    implemented a simple function that removes the first argument of a slice (os.Args)

--- main/main.go
+++ main/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
 )
 
@@ -12,12 +14,25 @@ type Task struct {
 	isCompleted bool
 }
 
+// removes the first argument (element) of a slice
+func removeFirstArg(args []string) ([]string, error) {
+	if len(args) > 1 {
+		return args[1:], nil // exclude the first argument
+	}
+
+	// returns an empty slice if no arguments are given
+	return []string{}, errors.New("removeFirstArgs func expects at least 2 arguments, but less were given")
+}
+
 func main() {
 
-	args := os.Args
+	args, err := removeFirstArg(os.Args)
 
-	for i := range args {
-
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
+
+	fmt.Println(args)
 
 }
