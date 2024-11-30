be9b10cfef68989a8f49fa319b79e9aa98ec8d61
Gustavo Daniel <gugadanielalvez@gmail.com>
2024-11-29 22:51:47 -0300

    implemented a simple add/list system to the tasklist

--- main/main.go
+++ main/main.go
@@ -8,9 +8,6 @@ import (
 
 type Task struct {
 	name        string
-	description string
-	importance  int
-	id          uint16
 	isCompleted bool
 }
 
@@ -24,6 +21,67 @@ func removeFirstArg(args []string) ([]string, error) {
 	return []string{}, errors.New("removeFirstArgs func expects at least 2 arguments, but less were given")
 }
 
+var commands = map[int]string{
+	1: "add",
+	2: "remove",
+	3: "list",
+	4: "change",
+}
+
+func validateCommand(args []string) (int, error) {
+	if len(args) >= 1 {
+		switch args[0] {
+		case "add":
+			return 1, nil
+		case "remove":
+			return 2, nil
+		case "list":
+			return 3, nil
+		case "change":
+			return 4, nil
+		}
+	}
+
+	return -1, errors.New("validateCommands expect an array of strings, but an empty one was given")
+}
+
+// global slice to store tasks
+var tasks []Task
+
+const MAX_TASKS int = 10
+
+// adds a task to the tasks slice
+func addTask(args []string) {
+
+	if len(args) >= MAX_TASKS {
+		fmt.Println("The task list is full! Cannot add more tasks.")
+		return
+	}
+
+	if len(args) < 2 {
+		fmt.Println("addTask expects a task name as an argument.")
+		return
+	}
+
+	// creates a new task and appends it to the slice
+	task := Task{name: args[1], isCompleted: false}
+	tasks = append(tasks, task)
+	fmt.Printf("Task \"%v\" added to the tasklist.\n", task.name)
+}
+
+func listTasks() {
+	if len(tasks) <= 0 {
+		fmt.Println("The list is empty.")
+		return
+	}
+
+	fmt.Println("Listing all tasks:")
+
+	for i := 1; i <= len(tasks); i++ {
+		fmt.Printf("%v. %v", i, tasks[i])
+	}
+}
+
 func main() {
 
 	args, err := removeFirstArg(os.Args)
@@ -33,6 +91,13 @@ func main() {
 		return
 	}
 
-	fmt.Println(args)
+	commandId, _ := validateCommand(args)
+
+	switch commandId {
+	case 1:
+		addTask(args)
+	case 3:
+		listTasks()
+	}
 
 }
